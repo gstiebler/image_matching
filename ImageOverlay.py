859488bc0ce815790537e8b0aad31c2cf228e760
Guilherme Stiebler <guilhermemst@gmail.com>
2017-03-31 16:40:12 -0300

    Using BFMatcher to match the keypoints, and using estimateRigidTransform to estimate the matrix

--- ImageOverlay.py
+++ ImageOverlay.py
@@ -6,11 +6,11 @@ BAR_LEN_FACTOR = 0.06
 
 imgs_folder = '../Images/'
 
-MovIm = cv2.imread(imgs_folder + 'g_009.tif', 0)
-FixIm = cv2.imread(imgs_folder + 'Slice69.tif', 0)
+MovIm = cv2.imread(imgs_folder + '001_CBS_010.jpg', 0)
+FixIm = cv2.imread(imgs_folder + '20141014-113042_1.jpg', 0)
 
 # Invert the image
-FixIm = 255 - FixIm
+#FixIm = 255 - FixIm
 
 FixImHeight = FixIm.shape[0]
 # calculate the height of the bar
@@ -25,7 +25,7 @@ FixIm = cv2.equalizeHist(FixIm)
 MovIm = cv2.equalizeHist(MovIm)
 
 # blur (window size must be odd)
-FixIm = cv2.GaussianBlur(FixIm, (41, 41), 10)
+# FixIm = cv2.GaussianBlur(FixIm, (41, 41), 10)
 
 # TODO resize?
 
@@ -52,10 +52,20 @@ bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 # Match descriptors.
 matches = bf.match(FixDes, MovDes)
 # Sort them in the order of their distance.
-matches = sorted(matches, key = lambda x:x.distance)
-# Draw first 10 matches.
-matches_im = drawMatches(FixIm, FixKp, MovIm, MovKp, matches[:10])
+sorted_matches = sorted(matches, key = lambda x:x.distance)
 
+# get the 10 best matches
+selected_matches = sorted_matches[:10]
+
+# get keypoints for selected matches
+selected_fix_kp = [list(FixKp[m.queryIdx].pt) for m in selected_matches]
+selected_mov_kp = [list(MovKp[m.trainIdx].pt) for m in selected_matches]
+
+selected_fix_kp = np.array([selected_fix_kp]).astype(np.int)
+selected_mov_kp = np.array([selected_mov_kp]).astype(np.int)
+
+# Draw the selected matches
+matches_im = drawMatches(FixIm, FixKp, MovIm, MovKp, selected_matches)
 
 # write the result images to file
 cv2.imwrite('FixIm.tif', FixIm)
@@ -63,3 +73,7 @@ cv2.imwrite('FixImKp.tif', FixImKp)
 cv2.imwrite('MovIm.tif', MovIm)
 cv2.imwrite('MovImKp.tif', MovImKp)
 cv2.imwrite('matches.tif', matches_im)
+
+# testing detecting the matching points automatically
+mat = cv2.estimateRigidTransform(selected_fix_kp, selected_mov_kp, True)
+print mat
