7d33b652eeb4c19d08690eab434fbeae60e6ddf7
Guilherme Stiebler <guilhermemst@gmail.com>
2017-03-31 10:52:36 -0300

    Reading and showing images

--- ImageOverlay.py
+++ ImageOverlay.py
@@ -1,1 +1,19 @@
-print "Hello"
+import numpy as np
+import cv2
+
+imgs_folder = '../Images/'
+
+MovIm = cv2.imread(imgs_folder + 'g_009.tif', 0)
+FixIm = cv2.imread(imgs_folder + 'Slice69.tif', 0)
+
+print MovIm.shape
+print FixIm.shape
+
+cv2.imshow(MovIm)
+
+print "teste"
+
+cv2.imwrite(MovIm, 'teste.jpg')
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
